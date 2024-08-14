1db5f3ec6a312a5f407f88df1e0f3325958d8293
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-08-14 22:18:43 +0530

    It is basic code of Arthmatic operators in which float is used

---
+++ Arthamtic.java
@@ -0,0 +1,10 @@
+public class Arthamtic{
+    public static void main(String[] args) {
+        int a=14,b=5;
+        float c=(float)a/b;
+        int r=a%b;
+
+        System.out.println(c);
+        System.out.println(r);
+    }
+}
