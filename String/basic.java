a86b9f00cc2ab965ecf3351884b908aea1a1a1eb
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-08-15 21:52:26 +0530

    basic program of string

---
+++ String/basic.java
@@ -0,0 +1,18 @@
+package String;
+
+public class lenght {
+ 
+        public static void main(String[] args) {
+          
+          // create strings
+          String first = "Java";
+          String second = "Python";
+          String third = "JavaScript";
+      
+          // print strings
+          System.out.println(first);   // print Java
+          System.out.println(second);  // print Python
+          System.out.println(third);   // print JavaScript
+        }
+    
+}
