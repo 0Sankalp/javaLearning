d1447bc26dff11eac9cc365d6a70fe77777a353e
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-08-16 22:13:33 +0530

    normal usde of if,else and else if

---
+++ conditonalStatement/condtion.java
@@ -0,0 +1,22 @@
+public class condtion {
+    
+    public static void main(String[] args) {
+        int a = 5, b=15,c=4;
+        if (a>b && a>c) 
+            
+        
+        {
+            System.out.println(a);
+
+        }
+        else if (b>c) {
+            System.out.println(b);
+            
+        }
+        else
+        {
+            System.out.println(c);
+
+        }
+    }
+}
