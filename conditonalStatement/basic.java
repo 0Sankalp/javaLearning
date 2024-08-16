cf1cb7de9d2f4937bea2c5d6cc5264dfda2cfa66
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-08-16 22:07:05 +0530

    Basic code of how to print a number posstive or negative

---
+++ conditonalStatement/basic.java
@@ -0,0 +1,21 @@
+package conditonalStatement;
+
+public class basic {
+
+    public static void main(String[] args) {
+        
+    
+    
+    int n=5;
+    if (n>=0)
+    {
+        System.out.println("Possitive");
+
+    }
+    else
+    {
+        System.out.println("Negative");
+    }
+    
+}
+}
