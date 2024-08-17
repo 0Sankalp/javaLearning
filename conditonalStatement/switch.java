0148e57fdce6cdbe690ee82c07fc4f4f9a517ef4
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-08-17 21:58:57 +0530

    example of switch case

---
+++ conditonalStatement/switch.java
@@ -0,0 +1,19 @@
+
+class Switch{
+    public static void main(String[] args)
+    {
+      char a = 'C';
+      switch (a)
+      {
+        case 'A':
+          System.out.println("Letter A");
+          break;
+        case 'C':
+          System.out.println("Letter C");
+          break;
+        default:
+          System.out.println("Default case: NO Letter Matched!");
+      }
+    }
+  }
+  
