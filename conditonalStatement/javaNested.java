cfbe695a916f80d9aed4a9987bea3184a74a3a64
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-08-17 22:24:44 +0530

    Example of nested condition

---
+++ conditonalStatement/javaNested.java
@@ -0,0 +1,20 @@
+public class javaNested {
+    //Java Program to demonstrate the use of Nested If Statement.    
+      
+public static void main(String[] args) {      
+    //Creating two variables for age and weight    
+    int age=25;    
+    int weight=48;      
+    //applying condition on age and weight    
+    if(age>=18){      
+        if(weight>50){    
+            System.out.println("You are eligible to donate blood");    
+        } else{  
+            System.out.println("You are not eligible to donate blood");    
+        }  
+    } else{  
+      System.out.println("Age must be greater than 18");  
+    }  
+} 
+ }  
+
