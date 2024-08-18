8c94923d4995c4403860aadad9afbb422f1b2491
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-08-18 21:57:02 +0530

    this is example of nested for loop,opposite pyramid

---
+++ loopConcept/pyramid2For.java
@@ -0,0 +1,15 @@
+package loopConcept;
+
+public class pyramid2For {
+    
+        public static void main(String[] args) {  
+        int term=6;  
+        for(int i=1;i<=term;i++){  
+        for(int j=term;j>=i;j--){  
+                System.out.print("* ");  
+        }  
+        System.out.println();//new line  
+        }  
+        }  
+        }  
+
