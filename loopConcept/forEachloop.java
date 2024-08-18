e166295489444d1c914c72e788f4989bcf4decae
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-08-18 21:52:40 +0530

    The for-each loop is used to traverse array or collection in Java

---
+++ loopConcept/forEachloop.java
@@ -0,0 +1,17 @@
+package loopConcept;
+
+public class forEachloop {
+    
+//Java For-each loop example which prints the  
+//elements of the array  
+
+    public static void main(String[] args) {  
+        //Declaring an array  
+        int arr[]={12,23,44,56,78};  
+        //Printing array using for-each loop  
+        for(int i:arr){  
+            System.out.println(i);  
+        }  
+    }  
+    }  
+
