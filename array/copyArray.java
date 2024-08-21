0acca2996e32a83c88fc8e2e178f282253184653
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-08-21 21:39:45 +0530

    java program to copy source array to destination array

---
+++ array/copyArray.java
@@ -0,0 +1,19 @@
+package array;
+
+public class copyArray {
+    //Java Program to copy a source array into a destination array in Java  
+ 
+    public static void main(String[] args) {  
+        //declaring a source array  
+        char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e',  
+                'i', 'n', 'a', 't', 'e', 'd' };  
+        //declaring a destination array  
+        char[] copyTo = new char[7];  
+        //copying array using System.arraycopy() method  
+        System.arraycopy(copyFrom, 2, copyTo, 0, 7);  
+        //printing the destination array  
+        System.out.println(String.valueOf(copyTo));  
+    }  
+}  
+    
+
