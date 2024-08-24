3830d7061327cf98565cbb227c8e5a7feca2b535
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-08-24 20:45:20 +0530

    basic example of for each  loop

---
+++ loopConcept/forEach.java
@@ -0,0 +1,15 @@
+package loopConcept;
+
+public class forEach {
+  
+        public static void main(String args[]){  
+         int arr[]={12,13,14,44};  
+         int total=0;  
+         for(int i:arr){  
+           total=total+i;  
+         }  
+        System.out.println("Total: "+total);  
+       }   
+      }  
+    
+
