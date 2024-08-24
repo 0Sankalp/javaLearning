3a5d4cc1fc8a6cb81c8c0b63ce9741fb93c76abe
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-08-24 20:47:33 +0530

    traversing the array element

---
+++ loopConcept/forEachbasic.java
@@ -0,0 +1,16 @@
+package loopConcept;
+
+public class forEachbasic {
+    //An example of Java for-each loop  
+ 
+    public static void main(String args[]){  
+     //declaring an array  
+     int arr[]={12,13,14,44};  
+     //traversing the array with for-each loop  
+     for(int i:arr){  
+       System.out.println(i);  
+     }  
+   }   
+  }  
+    
+
