7eeacbac08957e1d1af8492419c140ffaa20b00e
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-08-24 20:55:17 +0530

    Here you will found all the doWhile programs

---
+++ loopConcept/doWhile/basic.java
@@ -0,0 +1,14 @@
+package loopConcept.doWhile;
+
+public class basic {
+    
+   
+        public static void main(String[] args) {    
+            int i=1;    
+            do{    
+                System.out.println(i);    
+            i++;    
+            }while(i<=10);    
+        }    
+        }    
+
