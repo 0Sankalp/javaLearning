2e7c53ec36a4371a9bada622710315c171ac38ab
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-08-28 22:56:04 +0530

    Finding number is Armstrong,ex12321 is armstrong

---
+++ PracticeProblem/problem5.java
@@ -0,0 +1,23 @@
+package PracticeProblem;
+import java.util.Scanner;
+
+public class problem5 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter your number");
+        int  n=sc.nextInt();
+
+        int count=0;
+        while (n>0)
+         {
+            n=n/10;
+            count++;
+           
+            
+        }
+        System.out.println(count);
+
+    }
+    
+}
