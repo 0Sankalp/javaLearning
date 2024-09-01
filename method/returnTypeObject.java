ea54458cd47fcd22cacd9a29bfc5e930dfc95419
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-09-01 22:59:19 +0530

    Return variable

---
+++ method/returnTypeObject.java
@@ -0,0 +1,15 @@
+package method;
+
+public class returnTypeObject {
+    int max(int x,int y)
+    {
+    return x>y?x:y;
+    }
+    String userName(String email)
+    {
+        int a=email.indexOf("@");
+        String name=email.substring(o, a);
+        return name;
+    }
+    
+}
