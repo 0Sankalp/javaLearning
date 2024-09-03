f19a94c0f121a532a3cdeb078cd58d360c8cf684
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-09-03 23:50:42 +0530

    overladed method to reverse a int or array

---
+++ methodProblems/reverse.java
@@ -0,0 +1,22 @@
+// overladed method to reverse a int or array
+package methodProblems;
+
+public class reverse {
+    int reverse(int n)
+    {
+    int rev=0;
+
+    while (n!=0) 
+        {
+        rev=rev*10+n%10;
+        n=n/10;
+    }
+    return rev;
+    
+
+    
+}
+public static void main(String[] args) {
+    
+}
+}
