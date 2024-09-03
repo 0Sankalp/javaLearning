557f71d5a1565cd216f476571283231251e1fc13
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-09-03 23:44:24 +0530

    program to calculate the area

---
+++ methodProblems/areaCalculate.java
@@ -0,0 +1,19 @@
+package methodProblems;
+
+
+public class areaCalculate {
+    static double area(double radius)
+    {
+        return Math.PI*radius*radius;
+    }
+    static double area(double length,double breadth)
+    {
+        return length*breadth;
+    }    public static void main(String[] args) {
+        
+    }
+    
+}
+
+
+/*this is the parameter by which you can calculate the area */
