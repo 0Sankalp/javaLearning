1119d98de967aa92c4b30deb1691e5c27cfa63b6
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-09-05 00:41:34 +0530

    finding dicount using variable argument

---
+++ PracticeProblem/method.java
@@ -0,0 +1,30 @@
+package PracticeProblem;
+
+
+import java.util.Scanner;
+
+public class method {
+    
+
+public static void main (String args[])  
+{  
+//creating Scanner class object     
+Scanner scan=new Scanner(System.in);  
+System.out.print("Enter the number: ");  
+//reading value from user  
+int num=scan.nextInt();  
+//method calling  
+findEvenOdd(num);  
+}  
+//user defined method  
+public static void findEvenOdd(int num)  
+{  
+//method body  
+if(num%2==0)   
+System.out.println(num+" is even");   
+else   
+System.out.println(num+" is odd");  
+}  
+}  
+    
+
