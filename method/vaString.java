dd4b5c5c59cdfa9ad3abd14c487a890e3d0d857d
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-09-05 00:00:06 +0530

    adding array using variable arguments

---
+++ method/vaString.java
@@ -0,0 +1,20 @@
+public class vaString {
+
+        static void showList(String...S)
+        {
+            for(int i=0;i<S.length;i++)
+            {
+                System.out.println(i+1+". "+S[i]);
+                
+            }
+        }
+        public static void main(String[] args)
+         {
+            showList("Shreya","Aadisha","Shivam","Dwarika");
+             }
+    
+            }
+    
+    
+    
+
