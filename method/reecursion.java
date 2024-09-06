098e05e64c167f53f45eaf766aecb18b028247df
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-09-06 22:28:39 +0530

    example of recursion

---
+++ method/reecursion.java
@@ -0,0 +1,16 @@
+public class reecursion {
+    
+        static int count=0;  
+        static void p(){  
+        count++;  
+        if(count<=5){  
+        System.out.println("hello "+count);  
+        p();  
+        }  
+        }  
+        public static void main(String[] args) {  
+        p();  
+        }  
+        }  
+    
+
