159c944861221f4b834643da298f536988a9c3d2
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-09-08 22:36:19 +0530

    Recursion example

---
+++ method/infinteTimes.java
@@ -0,0 +1,13 @@
+public class infinteTimes {
+     
+        static void p(){  
+        System.out.println("hello");  
+        p();  
+        }  
+          
+        public static void main(String[] args) {  
+        p();  
+        }  
+        }  
+    
+
