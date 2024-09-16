254a47c0edc18ab3e8d2bc3daa3591a9e962fdfe
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-09-16 22:40:47 +0530

    This is a simple program to create an array and then to print it's all elements.

---
+++ Questions/printArray.java
@@ -0,0 +1,17 @@
+package Questions;
+
+public class printArray {
+ 
+        public static void main(String[] args) {  
+            //Initialize array  
+            int [] arr = new int [] {1, 2, 3, 4, 5};  
+            System.out.println("Elements of given array: ");  
+            //Loop through the array by incrementing value of i  
+            for (int i = 0; i < arr.length; i++) {  
+                System.out.print(arr[i] + " ");  
+            }  
+        }  
+    }  
+    
+
+//This is a simple program to create an array and then to print it's all elements.
