ad2a81920a7059e7d3c4b57cdd2d77db929ef23a
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-09-21 22:09:45 +0530

    We can use for loop to iterate through the values in a character array.

---
+++ arrayQuestion/characterArray.java
@@ -0,0 +1,13 @@
+package arrayQuestion;
+
+public class characterArray{
+
+    public static void main(String[] args) {  
+        char[] JavaCharArray = {'a', 'b', 'c', 'd', 'e'};  
+        for (char c:JavaCharArray) {  
+        System.out.println(c);  
+        }  
+    }  
+}  
+    
+
