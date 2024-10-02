2d0cb9c7ebcec13fee536dba9ad774ee6f7e0b95
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-10-02 21:39:13 +0530

    overload example

---
+++ OOPs_Concept/overloadMain.java
@@ -0,0 +1,15 @@
+package OOPs_Concept;
+
+public class overloadMain {
+    
+public static void main(int a)  //overloaded main method    
+{    
+System.out.println(a);    
+}    
+public static void main(String args[])    
+{       
+System.out.println("main method invoked");    
+main(6);    
+}    
+}    
+    
