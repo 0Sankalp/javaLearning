7d841df4269e31ea4cc2b3e5f98b121fb14227bd
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2026-03-10 01:16:56 +0530

    revsion

---
+++ java/loop.java
@@ -0,0 +1,50 @@
+
+// dowhile
+/*public class loop {
+    
+   
+        public static void main(String[] args) {    
+            int i=1;    
+            do{    
+                System.out.println(i);    
+            i++;    
+            }while(i<=10);    
+        }    
+        }    */
+
+        // for each loop
+
+    /*   public class loop {
+        
+            public static void main(String[] args) {
+                 int arr[]={12,13,14,44};  
+         int total=0;  
+         for(int i:arr){  
+           total=total+i;  
+         }  
+        System.out.println("Total: "+total);  
+       }   
+            }*/
+
+      /*  public class loop {
+       
+        public static void main(String[] args) {
+          
+        int i=1;  
+        while(i<=10){  
+            System.out.println(i);  
+        i++;  
+        }  
+    }  
+    } */ 
+        
+       public class loop {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
+        
