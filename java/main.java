8aa79cf39d1196c096a5f37940b15aebdf998792
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2026-03-09 23:42:41 +0530

    Revison0

---
+++ java/main.java
@@ -0,0 +1,71 @@
+
+
+/*public static void main(String[]args)
+{
+    System.out.println("Hello world");
+}
+}*/
+
+//simple if statement
+/*public static void main(String[] args) {
+    int x= 10;
+    int y=12;
+
+    if(x+y>20
+    ){
+        System.out.println("x+y is greater than 20");
+    }
+}*/
+// if else 
+   /*  public static void main(String[] args) {
+        int x= 10;
+        int y=12;
+            if(x+y<10){
+                System.out.println("x+y is less than 10");
+            }else{
+                System.out.println("x+y is greater than  20");
+            }
+    }
+  public class main{
+
+    public static void main(String[] args) {
+
+        String city = "Delhi";
+
+        if (city.equals("Meerut")) {
+            System.out.println("City is Meerut");
+        } 
+        else if (city.equals("Delhi")) {
+            System.out.println("City is Delhi");
+        } 
+        else if (city.equals("Noida")) {
+            System.out.println("City is Noida");
+        } 
+        else {
+            System.out.println(city);
+        }
+
+    }
+
+}*/
+public class main{
+    
+    public static void main(String[] args) {
+        
+        int num=2;
+
+        switch (num) {
+            case 0:
+                System.out.println("number is 0");
+                break;
+                
+        
+            case 1:
+            System.out.println("number is 1");
+                break;
+                default:
+                System.out.println(num);
+        }
+    }
+
+}
